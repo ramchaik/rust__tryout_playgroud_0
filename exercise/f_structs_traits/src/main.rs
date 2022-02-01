63bc7ac6dee3844801983549d8693455415d4699
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2022-02-01 09:56:26 +0530

    feat: structs & traits

---
+++ exercise/f_structs_traits/src/main.rs
@@ -0,0 +1,46 @@
+trait Bite {
+fn bite(self: &mut Self);
+}
+
+#[derive(Debug)] // This enables using the debugging format string "{:?}"
+struct Carrot {
+    percent_left: f32,
+}
+
+impl Bite for Carrot {
+    fn bite(self: &mut Self) {
+        self.percent_left *= 0.8;
+    }
+}
+
+#[derive(Debug)] // include this line right before your struct definition
+struct Grapes {
+    amount_left: i32,
+}
+
+impl Bite for Grapes {
+    fn bite(self: &mut Self) {
+        self.amount_left -= 1;
+    }
+}
+
+fn bunny_nibbles<T: Bite>(o: &mut T) {
+    for _ in 0..10 {
+        o.bite();
+    }
+}
+
+fn main() {
+    let mut carrot = Carrot { percent_left: 100.0 };
+    carrot.bite();
+    println!("I take a bite: {:?}", carrot);
+
+    let mut grapes = Grapes { amount_left: 100 };
+    grapes.bite();
+    println!("Eat a grape: {:?}", grapes);
+
+    bunny_nibbles(&mut carrot);
+    println!("Bunny nibbles for awhile: {:?}", carrot);
+}
+
+
